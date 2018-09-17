11298fdb353c311cbb29ec4d0a6ce86f5045a50f
Alvaro Mateus <alvaromateuz@yahoo.com.br>
2018-09-17 10:48:21 -0300

    Correção da solution

---
+++ 2-Domain/SGC.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.ApplicationCore.Entity
+{
+    public class Cliente
+    {
+        public int ClienteID { get; set; }
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+    }
+}
